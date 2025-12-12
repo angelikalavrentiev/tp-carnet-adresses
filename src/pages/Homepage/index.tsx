44a73914162b0a39d3d5ed6f714929a7e257bdbf
Valentin CORIOLLES <valentin.coriolles@3wacademy.fr>
2025-12-12 15:37:55 +0100

    feat : layout (creation du layout, Header et Footer)

---
+++ src/pages/Homepage/index.tsx
@@ -0,0 +1,17 @@
+import Footer from "../../layout/Footer";
+import Header from "../../layout/Header";
+
+const Homepage = () => {
+    return (
+        <>
+        <Header title="Bienvenue sur le carnet d'adresse" />
+
+        <main>
+            
+        </main>
+        <Footer content="© 2026 Carnet d'Adresses" />
+        </>
+    );
+}
+
+export default Homepage;
